7796b0184c30c102176a3d13afa7b43fc8c1b186
Wilson-Jara <wilson.jara@alumnos.uv.cl>
2026-01-15 23:11:41 -0300

    Ciclo While ejercicios propuestos

---
+++ src/Clase10/ProblemasPropuestos/Problema2.java
@@ -0,0 +1,25 @@
+package Clase10.ProblemasPropuestos;
+
+import java.util.Scanner;
+
+public class Problema2 {
+    public static void main(String[] ar) {
+        Scanner teclado=new Scanner(System.in);
+        int n,x;
+        float altura,suma,promedio;
+        System.out.print("Cuantas personas hay:");
+        n=teclado.nextInt();
+        x=1;
+        suma=0;
+        while (x<=n) {
+            System.out.print("Ingrese la altura:");
+            altura=teclado.nextFloat();
+            suma=suma + altura;
+            x=x + 1;
+        }
+        promedio=suma/n;
+        System.out.print("Altura promedio:");
+        System.out.print(promedio);
+    }
+}
+
