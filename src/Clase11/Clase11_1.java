0624a15e65e2d815b6dbbe7dd7e0bf2703ac9959
Wilson Alexis Jara Perez <wilson.jara@alumnos.uv.cl>
2026-01-28 03:26:00 -0300

    Clase 11 ciclo for (1 ejemplo)

---
+++ src/Clase11/Clase11_1.java
@@ -0,0 +1,12 @@
+//Realizar un programa que imprima en pantalla los números del 1 al 100.
+
+package Clase11;
+
+public class Clase11_1 {
+    public static void main(String[] ar) {
+        int f;
+        for(f=1 ; f<=100 ; f++) {
+            System.out.println(f);
+        }
+    }
+}
