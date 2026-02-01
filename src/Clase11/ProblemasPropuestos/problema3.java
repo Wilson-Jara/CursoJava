d32daa383e21d46a6d7341033160f0d4faa3c5a7
Wilson Alexis Jara Perez <wilson.jara@alumnos.uv.cl>
2026-01-31 23:23:51 -0300

    Problema 3 for

---
+++ src/Clase11/ProblemasPropuestos/problema3.java
@@ -0,0 +1,15 @@
+//Desarrollar un programa que muestre la tabla de multiplicar del 5 (del 5 al 50)
+
+package Clase11.ProblemasPropuestos;
+
+import java.util.Scanner;
+
+public class problema3 {
+    public static void main (String[]ar){
+        int f;
+
+        for (f=5 ; f<=50 ; f=f+5){
+            System.out.println(f);
+        }
+    }
+}
