e6f7691979e9f098b904c76823e8cd032689d211
Wilson-Jara <wilson.jara@alumnos.uv.cl>
2026-02-20 23:40:31 -0300

    Ejemplo 3

--- src/Clase12/Clase12_3.java
+++ src/Clase12/Clase12_3.java
@@ -1,4 +1,42 @@
+//Realizar un programa que permita ingresar el peso (en kilogramos) de piezas. El proceso termina cuando ingresamos
+//el valor 0. Se debe informar:
+//a) Cuántas piezas tienen un peso entre 9.8 Kg. y 10.2 Kg.?, cuántas con más de 10.2 Kg.? y cuántas con menos
+//de 9.8 Kg.?
+//b) La cantidad total de piezas procesadas.
+
 package Clase12;
 
+import java.util.Scanner;
+
 public class Clase12_3 {
+    public static void main(String[] ar) {
+        Scanner teclado=new Scanner(System.in);
+        int cant1,cant2,cant3,suma;
+        float peso;
+        cant1=0;
+        cant2=0;
+        cant3=0;
+        do {
+            System.out.print("Ingrese el peso de la pieza (0 pera finalizar):");
+            peso=teclado.nextFloat();
+            if (peso>10.2) {
+                cant1++;
+            } else {
+                if (peso>=9.8) {
+                    cant2++;
+                } else {
+                    if (peso>0) {
+                        cant3++;
+                    }
+                }
+            }
+        } while(peso!=0);
+        suma=cant1+cant2+cant3;
+        System.out.print("Piezas aptas:");
+        System.out.println(cant2);
+        System.out.print("Piezas con un peso superior a 10.2:");
+        System.out.println(cant1);
+        System.out.print("Piezas con un peso inferior a 9.8:");
+        System.out.println(cant3);
+    }
 }
